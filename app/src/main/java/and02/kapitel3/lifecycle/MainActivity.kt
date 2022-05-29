b2afb1be1bbd6079ef827520b5321c9ddf666bad
zanza89 <wantanboy89@gmail.com>
2022-05-29 16:11:29 +0200

    changed Text

--- app/src/main/java/and02/kapitel3/lifecycle/MainActivity.kt
+++ app/src/main/java/and02/kapitel3/lifecycle/MainActivity.kt
@@ -12,37 +12,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textview = findViewById<TextView>(R.id.textview_mainactivity)
-        textview.append("MainActivity -- \"on Create\" durchlaufen\n")
+        textview.append("MainActivity -- \"on Create\" Durchlaufen\n")
     }
 
     override fun onStart() {
         super.onStart()
         val textView = findViewById<TextView>(R.id.textview_mainactivity)
-        textView.append("MainActivity -- \"on Start\" durchlaufen\n")
+        textView.append("MainActivity -- \"on Start\" Durchlaufen\n")
     }
 
     override fun onResume() {
         super.onResume()
         val textView = findViewById<TextView>(R.id.textview_mainactivity)
-        textView.append("MainActivity -- \"on Resume\" durchlaufen\n")
+        textView.append("MainActivity -- \"on Resume\" Durchlaufen\n")
     }
 
     override fun onPause() {
         super.onPause()
         val textView = findViewById<TextView>(R.id.textview_mainactivity)
-        textView.append("MainActivity -- \"on Pause\" durchlaufen\n")
+        textView.append("MainActivity -- \"on Pause\" Durchlaufen\n")
     }
 
     override fun onStop() {
         super.onStop()
         val textView = findViewById<TextView>(R.id.textview_mainactivity)
-        textView.append("MainActivity -- \"on Stop\" durchlaufen\n")
+        textView.append("MainActivity -- \"on Stop\" Durchlaufen\n")
     }
 
     override fun onRestart() {
         super.onRestart()
         val textView = findViewById<TextView>(R.id.textview_mainactivity)
-        textView.append("MainActivity -- \"on Restart\" durchlaufen\n")
+        textView.append("MainActivity -- \"on Restart\" Durchlaufen\n")
     }
     fun onButtonZweiteActivityClick(view: View?){
         val textView = findViewById<TextView>(R.id.textview_mainactivity)
